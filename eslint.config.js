15abcb3480d295b04031c31c77e8ec6632504456
SSM <ssm102030@naver.com>
2025-04-01 16:42:02 +0900

    set : husky,Prettier 및 ESLint 설정 추가 및 패키지 업데이트

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +23,41 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'class',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^I[A-Z]',
+            match: false,
+          },
+        },
+        {
+          selector: 'variableLike',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+        },
+      ],
+      // ✅ JSDoc 스타일을 허용
+      'require-jsdoc': 'off', // 기본값 off지만 켜고 싶으면 설정 가능
+      // 기타 규칙
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  // ✅ Prettier 지원
+  {
+    rules: {
+      ...prettier.rules,
     },
   },
 )
