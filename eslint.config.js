a4dbfe418a93c10e23d39c734ead634cd7b4b0a5
SSM <ssm102030@naver.com>
2025-04-01 17:49:16 +0900

    set : 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,27 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import * as tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
-import * as tsPlugin from '@typescript-eslint/eslint-plugin';
 
-export default tseslint.config(
+export default await tseslint.config(
   {
     ignores: ['dist'],
   },
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser, // ⬅️ 이거 반드시 필요!
       parserOptions: {
-        project: './tsconfig.json', // 타입 정보 기준 tsconfig
-        tsconfigRootDir: process.cwd(), // 루트 디렉토리 설정
+        project: './tsconfig.app.json',
+        tsconfigRootDir: process.cwd(),
       },
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
+      '@typescript-eslint': tseslint.plugin, // ⬅️ 이것도 정확히 지정
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
