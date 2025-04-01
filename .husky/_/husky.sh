38a01cfa5befdb3152df1d7d6b2ca03461ba6d66
SSM <ssm102030@naver.com>
2025-04-01 17:23:20 +0900

    set : husky 재설정

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,20 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-# husky
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ -z "$husky_skip_init" ]; then
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
-
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name..."
-
-  if [ -f ~/.huskyrc ]; then
-    debug "sourcing ~/.huskyrc"
-    . ~/.huskyrc
-  fi
-
-  export readonly husky_skip_init=1
-  sh -e "$0" "$@"
-  exit $?
-fi
+They WILL FAIL in v10.0.0
+"
