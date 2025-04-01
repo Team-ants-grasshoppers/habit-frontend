70ad03456934da345139afaa2d5b981568c55462
SSM <ssm102030@naver.com>
2025-04-01 17:07:48 +0900

    set : husky 설정 수정

---
+++ .husky/_/husky.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# husky
+
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name..."
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  export readonly husky_skip_init=1
+  sh -e "$0" "$@"
+  exit $?
+fi
