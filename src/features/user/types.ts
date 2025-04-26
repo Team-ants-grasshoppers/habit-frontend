78f64799a030e3741e5a7654ca5f56a737cba0aa
SSM <ssm102030@naver.com>
2025-04-27 02:57:40 +0900

    feat: JoinedClub, ManagedClub, JoinedThunder 타입 추가

--- src/features/user/types.ts
+++ src/features/user/types.ts
@@ -3,4 +3,29 @@ export interface MyPageItem {
   title: string;
   imgUrl: string | null;
 }
+
 export type MyPageList = MyPageItem[];
+
+export interface JoinedClubItem {
+  Club_id: number;
+  title: string;
+  imgUrl: string | null;
+}
+
+export type JoinedClubList = JoinedClubItem[];
+
+export interface ManagedClubItem {
+  Club_id: number;
+  title: string;
+  imgUrl: string | null;
+}
+
+export type ManagedClubList = ManagedClubItem[];
+
+export interface JoinedThunderItem {
+  Club_id: number;
+  title: string;
+  imgUrl: string | null;
+}
+
+export type JoinedThunderList = JoinedThunderItem[];
