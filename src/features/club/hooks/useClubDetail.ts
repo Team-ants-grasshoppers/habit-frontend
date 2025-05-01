3cdb3c876b86d886960386743aa5f6b989f74dc9
SSM <ssm102030@naver.com>
2025-05-01 14:34:25 +0900

    fix: useClubDetail 훅에 userId 매개변수 추가 및 ClubDetailPage에서 사용

--- src/features/club/hooks/useClubDetail.ts
+++ src/features/club/hooks/useClubDetail.ts
@@ -6,22 +6,19 @@ import { separateMembersByRole } from '../../utils/separateMembersByRole';
 /** [Hook] 특정 모임의 상세 정보
  * - 모임 상세정보 + 멤버리스트
  */
-export const useClubDetail = (clubId?: string) => {
+export const useClubDetail = (clubId?: string, userId?: string) => {
   const defaultProfile = '/assets/default-profile.png';
 
   return useQuery({
-    queryKey: ['clubDetail', clubId],
+    queryKey: ['clubDetail', clubId, userId],
     queryFn: async ({ queryKey }) => {
       const [, clubId, userId] = queryKey;
       const detail = await fetchClubDetailApi(Number(clubId)); // 클럽상세 정보 API 호출
       const memberList = await fetchClubMembersApi(Number(clubId)); // 모임 회원 조회 API 호출 (배열)
-      console.log('✅ queryFn 응답 detail:', detail);
-      console.log('✅ queryFn 응답 memberList:', memberList);
       return { detail, memberList, userId };
     },
-    enabled: !!clubId, // clubId, userId가 모두 있을 때만 쿼리를 활성화함 (!!: boolean 변환)
+    enabled: !!clubId && !!userId, // clubId, userId가 모두 있을 때만 쿼리를 활성화함 (!!: boolean 변환)
     select: ({ detail, memberList, userId }): ClubDetailModel => {
-      console.log('✅ select 입력값:', detail, memberList, userId);
       const { admins, members, pendingUsers, isAdmin, isMember, isPending } = separateMembersByRole(
         memberList,
         userId || '',
@@ -32,7 +29,7 @@ export const useClubDetail = (clubId?: string) => {
         description: detail.description,
         category: detail.category,
         region: detail.region,
-        imageUrl: detail.imgUrl?.[0] ?? 'vite.svg',
+        imageUrl: detail.imgUrl,
         admins,
         members,
         pendingUsers: pendingUsers,
